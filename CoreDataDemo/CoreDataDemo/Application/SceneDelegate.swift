c2d31c6a70026978f6e04461b1ce612b9cfe502d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-24 11:08:41 +0300

    setup configure project

---
+++ CoreDataDemo/CoreDataDemo/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  CoreDataDemo
+//
+//  Created by Nikolai Maksimov on 24.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let rootVC = UINavigationController(rootViewController: PersonViewController())
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ CoreDataDemo/CoreDataDemo/Controllers/PersonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PersonViewController.swift
+//  CoreDataDemo
+//
+//  Created by Nikolai Maksimov on 24.01.2024.
+//
+
+import UIKit
+
+final class PersonViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
